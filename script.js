3da11988bf98146bbf1136e3c5c855ee4a537a59
CRbukoski <camerorb45@gmail.com>
2024-07-05 15:46:56 -0400

    added new sound effects

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+let play = document.getElementById("taskButton");
+function playSound() {
+let audio = new Audio("dowaa.wav");
+audio.play()
+}
+play.addEventListener("click", playSound);
 
+
+let play2 = document.querySelectorAll("checkbox");
+function playSound2() {
+let audio = new Audio("Achievement Sound Effect.mp3");
+audio.play2()
+}
+play2.addEventListener("click", playSound2);
 
 
 
