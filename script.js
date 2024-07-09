d3e0e06f88ca2ebe8e604aa6883a16d97a254473
CRbukoski <camerorb45@gmail.com>
2024-07-09 19:58:45 -0400

    CSS changes

--- script.js
+++ script.js
@@ -6,12 +6,25 @@ audio.play()
 play.addEventListener("click", playSound);
 
 
-let play2 = document.querySelectorAll("checkbox");
 function playSound2() {
-let audio = new Audio("Achievement Sound Effect.mp3");
-audio.play2()
+    let audio2 = new Audio("Achievement Sound Effect.mp3");
+    audio2.play();
+
 }
-play2.addEventListener("click", playSound2);
+
+let checkedBox = document.getElementById('checkbox');
+checkedBox.addEventListener('click', playSound2);
+
+
+let taskEl = document.getElementById('taskEl')
+let selectors = document.querySelectorAll('selectors')
+
+function addTask() {
+i = selectors
+for (let i = 0; i < 1; i++) 
+
+
+}
 
 
 
